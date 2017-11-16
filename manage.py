c1f2114e78748c7eb6e44e0cd43fc59d66076f7f
SarahShaffer <sshaffer@udel.edu>
2017-11-16 13:39:46 -0500

    made database

--- manage.py
+++ manage.py
@@ -1,8 +1,19 @@
 from flask_script import Manager
-from songbase import app
+from songbase import app, db, Artist
 
 manager = Manager(app)
 
+@manager.command
+def deploy():
+    db.drop_all()
+    db.create_all()
+    coldplay = Artist(name='Coldplay', about='Coldplay is a British rock band.')
+    maroon5 = Artist(name='Maroon 5', about='Maroon 5 is an American pop rock band.')
+    song1 = Song(name='yellow')
+    db.session.add(coldplay)
+    db.session.add(maroon5)
+    db.session.add(song1)
+    db.session.commit()
+
 if __name__ == '__main__':
     manager.run()
-    
